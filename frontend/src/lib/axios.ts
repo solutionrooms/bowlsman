9cae227b3b9d3e7d7036d208898f49cc596735b9
jonscott <jon.scott@solutionrooms.com>
2025-02-23 09:33:53 +0000

    Add axios configuration and update gitignore to allow src/lib

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token && config.headers) {
+    config.headers.Authorization = `Token ${token}`;
+  }
+  console.log("Request URL:", config.baseURL + config.url);
+  return config;
+});
+
+export default instance; 
